c7e35b8d9c032b23786853c6293e820af3b8367d
gordon <jinjenjun@gmail.com>
2026-05-05 18:08:28 +0800

    feat: add HomeController

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,31 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, usePage } from '@inertiajs/vue3';
+import ElInfoButton from '@/Components/ElInfoButton.vue';
+import '@/../scss/main.scss';
+
+const { auth } = usePage().props;
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <p>歡迎，{{ auth.user.name }}！</p>
+                        <p>你的角色：{{ auth.roles.join(', ') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
